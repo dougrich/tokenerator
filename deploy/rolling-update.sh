e966c51e5727dfbaab293c4a8b92b4b3a35003d4
Douglas Richardson <dougrich@outlook.com>
2019-05-27 20:43:09 -0700

    Removing beta

--- deploy/rolling-update.sh
+++ deploy/rolling-update.sh
@@ -6,4 +6,4 @@ gcloud compute instance-groups managed set-instance-template token-builder \
   --zone us-west1-a \
   --template token-builder-$TAG
 
-gcloud beta compute instance-groups managed rolling-action replace token-builder --quiet
+gcloud compute instance-groups managed rolling-action replace token-builder --zone us-west1-a
