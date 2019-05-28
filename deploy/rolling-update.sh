2979d6eab49c30ccf80c07c157e7354c9bafc2c4
Douglas Richardson <dougrich@outlook.com>
2019-05-27 20:31:21 -0700

    Added --quiet and region

--- deploy/rolling-update.sh
+++ deploy/rolling-update.sh
@@ -3,6 +3,7 @@
 TAG=$1
 
 gcloud compute instance-groups managed set-instance-template token-builder \
+  --region us-west1
   --template token-builder-$TAG
 
-gcloud beta compute instance-groups managed rolling-action replace token-builder
+gcloud beta --quiet compute instance-groups managed rolling-action replace token-builder
