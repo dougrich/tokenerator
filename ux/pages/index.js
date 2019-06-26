01ef66f75a3db924630bb1384932a01f342068d7
Douglas Richardson <dougrich@outlook.com>
2019-06-25 23:54:27 -0700

    Homepage content + fix to local development

--- ux/pages/index.js
+++ ux/pages/index.js
@@ -1,4 +1,14 @@
+import styled from '@emotion/styled'
 import Page from '../components/page'
+import { Container, Article } from '../components/styled'
+import * as HomepageDetails from '../src/homepage.md'
+
+const LogoImage = styled.img({
+  display: 'block',
+  margin: 'auto',
+  width: '20em',
+  height: '20em'
+})
 
 function Home (props) {
   return (
@@ -6,6 +16,13 @@ function Home (props) {
       user={props.user}
       canonical='https://tokens.dougrich.net/'
     >
+      <LogoImage
+        src='/static/logo.svg'
+        alt='Logo of the token builder'
+      />
+      <Container>
+        <Article dangerouslySetInnerHTML={{ __html: HomepageDetails }} />
+      </Container>
     </Page>
   )
 }
