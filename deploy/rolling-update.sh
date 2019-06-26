205d3abd7fbc13ef0d0afe35ee109237216f8ae9
Douglas Richardson <dougrich@outlook.com>
2019-06-26 10:32:57 -0700

    Fixed auto deploy scripts

--- deploy/rolling-update.sh
+++ deploy/rolling-update.sh
@@ -6,4 +6,6 @@ gcloud compute instance-groups managed set-instance-template token-builder \
   --zone us-west1-a \
   --template token-builder-$TAG
 
-gcloud --quiet beta compute instance-groups managed rolling-action replace token-builder --zone us-west1-a
+gcloud --quiet beta compute instance-groups managed rolling-action replace token-builder \
+  --zone us-west1-a \
+  --max-unavailable 0
